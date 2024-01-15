786fce83b4e59737a43aae50b230a84800dd8569
Sky Honnor <skyhonnor@skyhonnor.fr>
2024-01-15 13:32:46 +0100

    Upgrade CSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/templates/*.html","./src/**/static/js/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  "script":{"watch":"console.log('Test')"},
+}
+
